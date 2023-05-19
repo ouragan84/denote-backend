9e3647a61cda752093ee8d3a1a062aa101c97674
Edgar Baudry <edgar.baudry@gmail.com>
2023-05-18 19:15:25 -0700

    ban

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-// basic server in node.js with express, serves static html files from public folder
+// === IMPORTS ===
 
-// load the express module
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const { default: mongoose } = require('mongoose');
+const userSchema = require('./user');
+
+
+// === CONFIG ===
+
 
 require('dotenv').config();
 
@@ -19,6 +25,188 @@ process.on('uncaughtException', function (err) {
 });
 
 
+// === HANDLE USER ACTIONS ===
+
+
+// handle new user registration
+app.post('/register', async (req, res) => {
+    const platform = req.body.platform;
+    const timeCreated = Date.now()
+
+    const user = new userSchema({
+        platform: platform,
+        timeCreated: timeCreated,
+        events: [
+            {
+                time: timeCreated,
+                type: 'register',
+                aditionalData: ""
+            }
+        ],
+        email: "",
+        name: "",
+        lastTimeOpened: timeCreated,
+        timesUsedAI: 0,
+        bannedAI: false,
+        timeUnbannedAI: null,
+        isPaid: false,
+        timePaymentExpires: null
+    });
+
+    await user.save();
+
+    console.log('new user registered: ' + user._id)
+
+    res.send({userID: user._id});
+});
+
+// handle user opening the app
+app.post('/event', async (req, res) => {
+    const userID = req.body.userID;
+    const time = Date.now();
+    const type = req.body.type;
+    const aditionalData = req.body.aditionalData;
+
+    // get user from DB (if it doesn't exist, send error)
+    // findById no longer works with a callback
+    const user = await userSchema.findById(userID);
+    if(!user){
+        return res.send({error: 'user not found'});
+    }
+
+    user.events.push({
+        time: time,
+        type: type,
+        aditionalData: aditionalData
+    });
+
+    user.lastTimeOpened = time;
+
+    // save user
+    user.save();
+});
+
+// handle user requesting to pay
+app.post('/payment-request', async (req, res) => {
+    const userID = req.body.userID;
+    const time = Date.now();
+    const email = req.body.email;
+
+    const user = await userSchema.findById(userID);
+    if(!user){
+        return res.send({error: 'user not found'});
+    }
+
+    // update user
+    user.events.push({
+        time: time,
+        type: 'payment_request',
+        aditionalData: 'email: ' + email
+    });
+
+    // save user
+    user.save();
+
+    notifyAdmins('Payment request from ' + email + ' (user: ' + userID + ')');
+
+    return res.send({status: 'ok'});
+});
+
+// get aggregate data for all users, using our password
+app.post('/data', async (req, res) => {
+    if(req.body.password !== process.env.PASSWORD){
+        return res.status(400).send({error: 'wrong password'});
+    }
+
+    const allUsers = await userSchema.find({});
+    const data = {
+        totalUsers: allUsers.length,
+        totalPaidUsers: allUsers.filter((user) => user.isPaid).length,
+        totalBannedUsers: allUsers.filter((user) => user.bannedAI).length,
+        totalUnbannedUsers: allUsers.filter((user) => !user.bannedAI).length,
+        totalUsersUsingAI: allUsers.filter((user) => user.timesUsedAI > 0).length,
+        totalUsersUsingAIInLast24Hours: allUsers.filter((user) => user.timesUsedAI > 0 && user.lastTimeOpened > Date.now() - 24*60*60*1000).length,
+        totalUsersUsingAIInLast7Days: allUsers.filter((user) => user.timesUsedAI > 0 && user.lastTimeOpened > Date.now() - 7*24*60*60*1000).length,
+        totalUsersUsingAIInLast30Days: allUsers.filter((user) => user.timesUsedAI > 0 && user.lastTimeOpened > Date.now() - 30*24*60*60*1000).length,
+        totalUsersActiveInLast24Hours: allUsers.filter((user) => user.lastTimeOpened > Date.now() - 24*60*60*1000).length,
+        totalUsersActiveInLast7Days: allUsers.filter((user) => user.lastTimeOpened > Date.now() - 7*24*60*60*1000).length,
+        totalUsersActiveInLast30Days: allUsers.filter((user) => user.lastTimeOpened > Date.now() - 30*24*60*60*1000).length,
+        users: allUsers
+    }
+
+    res.send(data);
+});
+
+// ban a user from using the AI for set time, using our password
+app.post('/ban', async (req, res) => {
+    if(req.body.password !== process.env.PASSWORD){
+        return res.status(400).send({error: 'wrong password'});
+    }
+
+    const userID = req.body.userID;
+    const time = Date.now();
+    const timeToBan = req.body.timeToBan; // in days
+    const timeUnbannedAI = new Date(time + timeToBan*24*60*60*1000);
+
+    const user = await userSchema.findById(userID);
+    if(!user){
+        return res.send({error: 'user not found'});
+    }
+
+    // update user
+    user.bannedAI = true;
+    user.timeUnbannedAI = timeUnbannedAI;
+
+    user.save();
+
+    return res.send({status: 'ok'});
+});
+
+// unban a user from using the AI, using our password
+app.post('/unban', async (req, res) => {
+    if(req.body.password !== process.env.PASSWORD){
+        return res.status(400).send({error: 'wrong password'});
+    }
+
+    if(req.body.password !== process.env.PASSWORD){
+        return res.status(400).send({error: 'wrong password'});
+    }
+
+    const userID = req.body.userID;
+
+    const user = await userSchema.findById(userID);
+    
+    if(!user){
+        return res.send({error: 'user not found'});
+    }
+
+    // update user
+    user.bannedAI = false;
+    user.timeUnbannedAI = null;
+
+    user.save();
+
+    return res.send({status: 'ok'});
+});
+
+// set a user as paid, using our password
+app.post('/set-paid', async (req, res) => {
+    if(req.body.password !== process.env.PASSWORD){
+        return res.status(400).send({error: 'wrong password'});
+    }
+});
+
+// set a user as unpaid, using our password
+app.post('/set-unpaid', async (req, res) => {
+    if(req.body.password !== process.env.PASSWORD){
+        return res.status(400).send({error: 'wrong password'});
+    }
+});
+
+
+// === AI API ===
+
+
 const OPEN_AI_KEY = process.env.OPEN_AI_KEY;
 
 const getBody = (initial, prompt, question) => {
@@ -29,7 +217,7 @@ const getBody = (initial, prompt, question) => {
             {role: 'system', content: prompt},
             {role: 'user', content: question}
         ]
-      }
+    }
 } 
 
 const readFile = (name) => {
@@ -65,21 +253,42 @@ const callGPT = async (promptTitle, question) => {
 // TODO: add authorization and ai-token counting
 app.post('/ai/:promptTitle', async (req, res) => {
     const promptTitle = req.params.promptTitle;
-    console.log('Prompt: ' + promptTitle);
+    const userID = req.body.userID;
 
 
-    console.log(req.body )
+    const user = await userSchema.findById(userID);
+    if(!user){
+        return res.send({error: 'user not found'});
+    }
+
+    // chec k if user is banned
+    if(user.bannedAI){
+        return res.send({error: 'user banned'});
+    }
+
+    // update user
+    user.lastTimeOpened = Date.now();
+    user.timesUsedAI += 1;
+    user.events.push({
+        time: Date.now(),
+        type: 'ai_request',
+        aditionalData: promptTitle,
+    });
+
+    // save user
+    user.save();
+
+    // console.log(req.body)
     const question = req.body.question;
 
-    // q: why is req.body undefined?
-    // a: https://stackoverflow.com/questions/38306569/express-bodyparser-req-body-undefined
-
-
-
     const response = await callGPT(promptTitle, question);
-    console.log('response', response);
+    // console.log('response', response);
     res.send(response);
 });
+
+
+// === SERVING STATIC FILES ===
+
 
 // set the static files location /public/img will be /img for users
 app.use(express.static(path.join(__dirname, 'public'))); 
@@ -89,6 +298,41 @@ app.get('/', function(req, res) {
     res.render('home', {url: process.env.URL, version: process.env.VERSION});
 });
 
-app.listen(process.env.PORT, () => {
-    console.log('Our app is running on http://localhost:' + process.env.PORT);
+app.get('/about', function(req, res) {
+    res.render('about', {url: process.env.URL});
 });
+
+
+
+// === ADMIN NOTIFICATIONS ===
+
+
+const notifyAdmins = (message) => {
+    console.log('notify admins: ' + message);
+}
+
+
+// === MONGO DB and START SERVER ===
+
+const DB_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+    
+const connectDB = async () => {
+    mongoose.set('strictQuery', false);
+    try{
+        await mongoose.connect(DB_URL, {});
+    }catch (err){
+        console.error(err);
+    }
+}
+
+// connect DB
+connectDB();
+
+// start listening for connections
+mongoose.connection.once('open', () => {
+    console.log('🌿 Connected to mongoDB')
+    const httpServer = app.listen(process.env.PORT, ()=>{
+        console.log('🚀 Server started at ' + process.env.URL);
+
+    });
+})
---
+++ user.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    email:{
+        type: String,
+    },
+    name:{
+        ttype: String,
+    },
+    lastTimeOpened:{
+        type: Date,
+    },
+    events: [
+        {
+            time: {
+                type: Date,
+            },
+            type: {
+                type: String,
+            },
+            aditionalData: {
+                type: String
+            }
+        }
+    ],
+    timeCreated: {
+        type: Date,
+    },
+    timesUsedAI: {
+        type: Number,
+        default: 0
+    },
+    platform: {
+        type: String,
+        required: true
+    },
+    timeUnbannedAI: {
+        type: Date,
+        default: null
+    },
+    bannedAI: {
+        type: Boolean,
+        default: false
+    },
+    isPaid: {
+        type: Boolean,
+        default: false
+    },
+    timePaymentExpires: {
+        type: Date,
+        default: null
+    },
+})
+
+module.exports = mongoose.model('DenoteBetaUser', userSchema)
